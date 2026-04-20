c46057b47be23ecb415986e7b164e9e91a66f27d
Ahoo Wang <ahoowang@qq.com>
2026-04-20 22:27:36 +0800

    refactor(core): LoadBalancerRegistry as interface with DefaultLoadBalancerRegistry impl

--- core/src/main/kotlin/me/ahoo/cobal/LoadBalancerRegistry.kt
+++ core/src/main/kotlin/me/ahoo/cobal/LoadBalancerRegistry.kt
@@ -1,10 +1,20 @@
 package me.ahoo.cobal
 
-class LoadBalancerRegistry {
+interface LoadBalancerRegistry {
+    fun <NODE : Node> getOrCreate(id: LoadBalancerId, factory: () -> LoadBalancer<NODE>): LoadBalancer<NODE>
+
+    fun <NODE : Node> get(id: LoadBalancerId): LoadBalancer<NODE>?
+
+    fun remove(id: LoadBalancerId): LoadBalancer<*>?
+
+    fun contains(id: LoadBalancerId): Boolean
+}
+
+class DefaultLoadBalancerRegistry : LoadBalancerRegistry {
     private val registry = java.util.concurrent.ConcurrentHashMap<LoadBalancerId, LoadBalancer<*>>()
 
     @Suppress("UNCHECKED_CAST")
-    fun <NODE : Node> getOrCreate(id: LoadBalancerId, factory: () -> LoadBalancer<NODE>): LoadBalancer<NODE> {
+    override fun <NODE : Node> getOrCreate(id: LoadBalancerId, factory: () -> LoadBalancer<NODE>): LoadBalancer<NODE> {
         registry[id]?.let { return it as LoadBalancer<NODE> }
         return synchronized(this) {
             registry[id]?.let { return@synchronized it as LoadBalancer<NODE> }
@@ -13,15 +23,15 @@ class LoadBalancerRegistry {
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun <NODE : Node> get(id: LoadBalancerId): LoadBalancer<NODE>? {
+    override fun <NODE : Node> get(id: LoadBalancerId): LoadBalancer<NODE>? {
         return registry[id] as LoadBalancer<NODE>?
     }
 
-    fun remove(id: LoadBalancerId): LoadBalancer<*>? {
+    override fun remove(id: LoadBalancerId): LoadBalancer<*>? {
         return registry.remove(id)
     }
 
-    fun contains(id: LoadBalancerId): Boolean {
+    override fun contains(id: LoadBalancerId): Boolean {
         return registry.containsKey(id)
     }
 }

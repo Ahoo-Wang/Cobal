faee6ac11900cff8020529b02ab012896d0c74dd
Ahoo Wang <ahoowang@qq.com>
2026-04-21 16:51:12 +0800

    refactor(langchain4j): replace manual error conversion with LangChain4jErrorConverter
    - Remove unused imported error classes from me.ahoo.cobal package
    - Replace manual error conversion logic with LangChain4jErrorConverter.convert method
    - Remove deprecated companion object with toNodeError function

--- langchain4j/src/main/kotlin/me/ahoo/cobal/langchain4j/LoadBalancedStreamingChatModel.kt
+++ langchain4j/src/main/kotlin/me/ahoo/cobal/langchain4j/LoadBalancedStreamingChatModel.kt
@@ -4,13 +4,7 @@ import dev.langchain4j.model.chat.StreamingChatModel
 import dev.langchain4j.model.chat.response.ChatResponse
 import dev.langchain4j.model.chat.response.StreamingChatResponseHandler
 import me.ahoo.cobal.AllNodesUnavailableError
-import me.ahoo.cobal.AuthenticationError
-import me.ahoo.cobal.CobalError
-import me.ahoo.cobal.InvalidRequestError
 import me.ahoo.cobal.LoadBalancer
-import me.ahoo.cobal.RateLimitError
-import me.ahoo.cobal.ServerError
-import me.ahoo.cobal.TimeoutError
 import me.ahoo.cobal.langchain4j.model.StreamingChatModelNode
 
 class LoadBalancedStreamingChatModel(
@@ -40,8 +34,7 @@ class LoadBalancedStreamingChatModel(
             }
 
             override fun onError(error: Throwable) {
-                val nodeError =
-                    toNodeError(selected.node.id, error as? Exception ?: RuntimeException(error.message, error))
+                val nodeError = LangChain4jErrorConverter.convert(selected.node.id, error)
                 selected.onFailure(nodeError)
                 doChatWithRetry(prompt, handler, remainingRetries - 1)
             }
@@ -54,18 +47,6 @@ class LoadBalancedStreamingChatModel(
             val nodeError = LangChain4jErrorConverter.convert(selected.node.id, e)
             selected.onFailure(nodeError)
             doChatWithRetry(prompt, handler, remainingRetries - 1)
-        }
-    }
-
-    companion object {
-        fun toNodeError(nodeId: String, e: Exception): CobalError {
-            return when (e) {
-                is dev.langchain4j.exception.RateLimitException -> RateLimitError(nodeId, e)
-                is dev.langchain4j.exception.InvalidRequestException -> InvalidRequestError(nodeId, e)
-                is dev.langchain4j.exception.AuthenticationException -> AuthenticationError(nodeId, e)
-                is dev.langchain4j.exception.TimeoutException -> TimeoutError(nodeId, e)
-                else -> ServerError(nodeId, e)
-            }
         }
     }
 }
